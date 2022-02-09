b273255b433bee3a8eb6bdff6a105a381411a96c
aidar <aidarmyrza@gmail.com>
2022-02-09 16:54:39 +0600

    search pagination replies

--- stack_api/urls.py
+++ stack_api/urls.py
@@ -18,10 +18,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from main.views import ProblemViewSet
+from main.views import ProblemViewSet, ReplyViewSet, CommentViewSet
 
 router = DefaultRouter()
 router.register('problems', ProblemViewSet)
+router.register('replies', ReplyViewSet)
+router.register('comments', CommentViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
