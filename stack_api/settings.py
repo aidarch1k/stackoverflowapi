b1db440acf22b9088a68fe2a2a751f2b435138c2
aidar <aidarmyrza@gmail.com>
2022-02-11 13:02:36 +0600

    Likes, Token, Swagger, Permissions

--- stack_api/settings.py
+++ stack_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+from datetime import timedelta
 from pathlib import Path
 from decouple import config
 
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     #mylibs
     'rest_framework',
     'phonenumber_field',
+    'drf_yasg',
 
     #myapps
     'main',
@@ -139,7 +141,14 @@ AUTH_USER_MODEL = 'account.CustomUser'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2
+    'PAGE_SIZE': 2,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=31),
 }
 
 TWILIO_ACCOUNT_SID = config('TWILIO_ACCOUNT_SID')
